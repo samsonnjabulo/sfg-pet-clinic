99c8534be7e9a44414f655130de5c24601221e18
samson <samson.shongwe@fnb.co.za>
2020-09-27 13:37:48 +0200

    initial project add

---
+++ src/main/java/za/co/fnb/nav/is/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package za.co.fnb.nav.is.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/fnb/nav/is/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.fnb.nav.is.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
